19c49e7a6843f2f56b20fe8f79bc66bc2b5aebe9
Ti-webdev <ror@3apaxi.com>
2012-12-15 15:06:57 +0600

    issues 2: GET routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Badgenator::Application.routes.draw do
-  match 'contacts' => 'welcome#contacts', :as => :contacts
-  match 'archive' => 'welcome#archive'
-  match 'archive/new' => 'badge_sets#create'
-  
   root :to => 'welcome#index'
+
+  match '/contacts' => 'welcome#contacts', :as => :contacts
+  match '/archive' => 'badge_sets#index'
+  match '/archive/page/:page' => 'badge_sets#index'
+  match '/archive/new' => 'badge_sets#new'
+  match '/archive/:id/edit' => 'badge_sets#edit'
+  
+  match '/archive/:badge_set_id/badges' => 'badges#index'
+  match '/archive/:badge_set_id/badges/page/:page' => 'badges#index'
+  match '/archive/:badge_set_id/print' => 'badge_sets#print'
+  match '/archive/:badge_set_id/badges/new' => 'badges#new'
+  match '/archive/:badge_set_id/badges/:id' => 'badges#show'
+  match '/archive/:badge_set_id/badges/:id/edit' => 'badge_sets#edit'
 end
